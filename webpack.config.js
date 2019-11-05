89859ad82bcc45a07d8a0c658ffefd65d65f9bb7
Tony Green <tonygreen@notonthehighstreet.com>
2019-11-05 17:53:26 +0000

    fix by removing node externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   target: 'node',
-  externals: [nodeExternals()],
   devtool: "inline-cheap-module-source-map",
   module: {
     rules: [
